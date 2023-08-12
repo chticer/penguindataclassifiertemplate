6e827ea36a7fe989fde38c31211fb778057d74d8
chticer <chticer@outlook.com>
2023-08-11 21:11:30 -0500

    Add penguin data classifier files

---
+++ app.py
@@ -0,0 +1,44 @@
+from # import the Flask, render_template, request, and url_for classes from the flask package
+import # import the tensorflow package with an alias of tf
+import # import the numpy package with an alias of np
+import # import the os package
+
+app = Flask(__name__, static_url_path = "/assets", static_folder = "assets")
+
+model_directory = "" # set the model directory
+
+classes = [ "Adelie", "Chinstrap", "Gentoo" ]
+
+@app. # set the index.html route to /
+def index():
+    model_files = []
+
+    for current_file in os.listdir(model_directory):
+        model_files.append(os.path.join(model_directory, current_file))
+
+    model_files.sort(key = os.path.getmtime, reverse = True)
+
+    return render_template("", model_options = "".join(f"<option value=\"{os.path.basename(current_model_file)}\">{os.path.basename(current_model_file)}</option>" for current_model_file in model_files)) # render the index.html webpage
+
+@app. # set the predict.html route to /predict that accepts a POST request
+def predict():
+    # Fill in the missing features from the form fields.
+    features_form = [
+        float(request.form["culmen-length"]),
+        ,
+        ,
+
+    ]
+
+    model = tf.keras.models.load_model(os.path.join("models", )) # load the selected model
+
+    class_probabilities =  # predict the model with a numpy-based array of the features
+
+    best_class_index = np.argmax(class_probabilities, axis = 1)[0]
+
+    class_predicted = classes[best_class_index]
+
+    return ("", class_predicted = , class_image_url = url_for("static", filename = f"{}.jpg")) # render the predict.html webpage with the predicted class and the lowercased file name of the predicted class
+
+if __name__ == "__main__":
+    app.(host = ) # run the Flask app server on IP address 0.0.0.0
